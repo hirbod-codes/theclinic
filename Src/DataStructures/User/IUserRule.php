11ed9012b195fde3ebdc97e3e04be33b236e5cee
Hirbod <hirbod.khatami.words@gmail.com>
2022-02-21 00:17:58 -0800

    Bug fix IUserRule interface
    Renamed privilegesExists method to privilegeExists.
    Forgot the argument for the privilegeExists method

--- Src/DataStructures/User/IUserRule.php
+++ Src/DataStructures/User/IUserRule.php
@@ -5,7 +5,7 @@ namespace TheClinic\DataStructures\User;
 interface IUserRule
 {
     public function getName(): string;
-    public function privilegesExists(): bool;
+    public function privilegeExists(string $privilege): bool;
     public function getPrivileges(): array;
     public function getPrivilegeValue(string $privilege): mixed;
 }
