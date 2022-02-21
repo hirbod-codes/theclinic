4e370bd3c8f9547c1b3b81742e29fcdef4048ee6
Hirbod <hirbod.khatami.words@gmail.com>
2022-02-21 03:15:04 -0800

    Update DSOrderTest test class
    New test class for DSOrder class.

--- Tests/DataStructures/Order/DSOrderTest.php
+++ Tests/DataStructures/Order/DSOrderTest.php
@@ -7,11 +7,8 @@ use Faker\Generator;
 use Mockery;
 use PHPUnit\Framework\TestCase;
 use TheClinic\DataStructures\Order\DSOrder;
-use TheClinic\DataStructures\Order\DSPackages;
-use TheClinic\DataStructures\Order\DSParts;
 use TheClinic\DataStructures\User\DSUser;
 use TheClinic\DataStructures\Visit\DSVisits;
-use TheClinic\Exceptions\DataStructures\Order\OrderExceptions;
 
 class DSOrderTest extends TestCase
 {
@@ -24,20 +21,9 @@ class DSOrderTest extends TestCase
 
     public function testDataStructure(): void
     {
-        $gender = "Male";
-
         /** @var DSUser|\Mockery\MockInterface $user */
         $user = Mockery::mock(DSUser::class);
-        $user->shouldReceive('getGender')->andReturn($gender);
         $user->shouldReceive('getId')->andReturn(56);
-
-        /** @var DSParts|\Mockery\MockInterface $parts */
-        $parts = Mockery::mock(DSParts::class);
-        $parts->shouldReceive('getGender')->andReturn($gender);
-
-        /** @var DSPackages|\Mockery\MockInterface $packages */
-        $packages = Mockery::mock(DSPackages::class);
-        $packages->shouldReceive('getGender')->andReturn($gender);
 
         $price = $this->faker->numberBetween(100, 600000);
         $time = $this->faker->numberBetween(100, 7200);
@@ -50,75 +36,17 @@ class DSOrderTest extends TestCase
         $this->runTheAssertions(
             $id,
             $user,
-            $parts,
-            $packages,
             null,
             $price,
             $time,
             $createdAt,
             $updatedAt
         );
-
-        try {
-            $user->shouldReceive('getGender')->andReturn("Female");
-
-            $this->runTheAssertions(
-                $id,
-                $user,
-                $parts,
-                $packages,
-                null,
-                $price,
-                $time,
-                $createdAt,
-                $updatedAt
-            );
-        } catch (OrderExceptions $th) {
-            $user->shouldReceive('getGender')->andReturn($gender);
-        }
-
-        try {
-            $parts->shouldReceive('getGender')->andReturn("Female");
-
-            $this->runTheAssertions(
-                $id,
-                $user,
-                $parts,
-                $packages,
-                null,
-                $price,
-                $time,
-                $createdAt,
-                $updatedAt
-            );
-        } catch (OrderExceptions $th) {
-            $parts->shouldReceive('getGender')->andReturn($gender);
-        }
-
-        try {
-            $packages->shouldReceive('getGender')->andReturn("Female");
-
-            $this->runTheAssertions(
-                $id,
-                $user,
-                $parts,
-                $packages,
-                null,
-                $price,
-                $time,
-                $createdAt,
-                $updatedAt
-            );
-        } catch (OrderExceptions $th) {
-            $packages->shouldReceive('getGender')->andReturn($gender);
-        }
     }
 
     private function runTheAssertions(
         int $id,
         DSUser $user,
-        DSParts $parts,
-        DSPackages $packages,
         DSVisits|null $visits = null,
         int $price,
         int $time,
@@ -128,8 +56,6 @@ class DSOrderTest extends TestCase
         $dsOrder = new DSOrder(
             $id,
             $user,
-            $parts,
-            $packages,
             $visits = null,
             $price,
             $time,
@@ -139,8 +65,6 @@ class DSOrderTest extends TestCase
 
         $this->assertEquals($dsOrder->getId(), $id);
         $this->assertEquals($dsOrder->getUser()->getId(), $user->getId());
-        $this->assertEquals($dsOrder->getParts(), $parts);
-        $this->assertEquals($dsOrder->getPackages(), $packages);
         $this->assertEquals($dsOrder->visits, $visits);
         $this->assertEquals($dsOrder->getPrice(), $price);
         $this->assertEquals($dsOrder->getNeededTime(), $time);
