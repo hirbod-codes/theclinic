35c85e4ea3f1bd0c5bdf8e9f32a85247cc856a75
Hirbod <hirbod.khatami.words@gmail.com>
2022-02-23 22:24:48 -0800

    Update ICalculateLaserOrder interface
    From now on needed information for price and time calculation should be passed through method parameters instead of class __construct method.
    so we can test it's clients easier.

--- Src/Order/ICalculateLaserOrder.php
+++ Src/Order/ICalculateLaserOrder.php
@@ -2,9 +2,17 @@
 
 namespace TheClinic\Order;
 
+use TheClinic\DataStructures\Order\DSPackages;
+use TheClinic\DataStructures\Order\DSParts;
 use TheClinic\Order\ICalculateOrder;
+use TheClinic\Order\Laser\ILaserPriceCalculator;
+use TheClinic\Order\Laser\ILaserTimeConsumptionCalculator;
 
 interface ICalculateLaserOrder extends ICalculateOrder
 {
-    public function calculatePriceWithoutDiscount(): int;
+    public function calculatePrice(DSParts|null $parts = null, DSPackages|null $packages = null, ILaserPriceCalculator $priceCalculator): int;
+
+    public function calculateTimeConsumption(DSParts|null $parts = null, DSPackages|null $packages = null, ILaserTimeConsumptionCalculator $timeConsumptionCalculator): int;
+
+    public function calculatePriceWithoutDiscount(DSParts|null $parts = null, DSPackages|null $packages = null, ILaserPriceCalculator $priceCalculator): int;
 }
