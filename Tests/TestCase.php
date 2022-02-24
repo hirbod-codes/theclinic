b25ec1e37fc03266e6eaa83c80bf5ef857663593
Hirbod <hirbod.khatami.words@gmail.com>
2022-02-23 22:33:18 -0800

    Create TestCase class

---
+++ Tests/TestCase.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase as BaseTestCase;
+use TheClinic\App\ServiceContainer\ServiceContainer;
+
+class TestCase extends BaseTestCase
+{
+    protected function setUp(): void
+    {
+        // ServiceContainer::getContainer()->bootServiceProviders();
+    }
+}
