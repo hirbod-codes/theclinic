70b5f8e44d30f98931c70fd4184764477f08c49f
Hirbod <hirbod.khatami.words@gmail.com>
2022-04-08 06:54:07 -0700

    Refactor SearchingBetweenDownTimes class

--- Src/Visit/Utilities/SearchingBetweenTimeRange.php
+++ Src/Visit/Utilities/SearchingBetweenTimeRange.php
@@ -5,14 +5,17 @@ namespace TheClinic\Visit\Utilities;
 use TheClinicDataStructures\DataStructures\Visit\DSVisits;
 use TheClinic\Exceptions\Visit\NeededTimeOutOfRange;
 use TheClinic\Exceptions\Visit\VisitSearchFailure;
+use TheClinicDataStructures\DataStructures\Visit\DSVisit;
 
 class SearchingBetweenTimeRange
 {
     public function search(int $firstTS, int $lastTS, int $consumingTime, DSVisits $futureVisits): int
     {
-        if (($lastTS - $firstTS) < $consumingTime) {
-            throw new NeededTimeOutOfRange("Incorrect time range(" . strval($firstTS - $lastTS) . ") for requested time consumption(" . strval($consumingTime) . ".)", 500);
-        }
+        // For testing purposes
+        $firsDT = (new \DateTime)->setTimestamp($firstTS);
+        $lastDT = (new \DateTime)->setTimestamp($lastTS);
+
+        $this->validateTimeRange($firstTS, $lastTS, $consumingTime);
 
         if (count($futureVisits) === 0) {
             return $firstTS;
@@ -20,29 +23,66 @@ class SearchingBetweenTimeRange
 
         /** @var \TheClinicDataStructures\DataStructures\Visit\DSVisit $visit */
         foreach ($futureVisits as $visit) {
-            if ($visit->getVisitTimestamp() < $firstTS && $visit->getVisitTimestamp() < $lastTS) {
+            $visitEnd = $visit->getVisitTimestamp() + $visit->getConsumingTime();
+
+            if ($visit->getVisitTimestamp() <= $firstTS && $visitEnd >= $lastTS) {
+                throw new VisitSearchFailure("Failed to find a visit in requested time range.", 500);
+            }
+
+            if ($visitEnd <= $firstTS) {
+                continue;
+            } elseif ($visit->getVisitTimestamp() <= $firstTS) {
+                $firstTS = $visitEnd;
+                $this->validateTimeRange($firstTS, $lastTS, $consumingTime);
                 continue;
             }
 
-            if ($futureVisits->key() === 0 && $visit->getVisitTimestamp() > $firstTS && ($visit->getVisitTimestamp() - $firstTS) >= $consumingTime) {
-                return $firstTS;
+            if (isset($previousVisit)) {
+                $previousBlock = $previousVisit->getVisitTimestamp() + $previousVisit->getConsumingTime();
+            } else {
+                $previousBlock = $firstTS;
             }
 
-            $futureVisits->prev();
-            if ($futureVisits->valid()) {
-                $previousVisit = $futureVisits->current();
-
-                if (($visit->getVisitTimestamp() - ($previousVisit->getVisitTimestamp() + $previousVisit->getConsumingTime())) >= $consumingTime) {
-                    return $previousVisit->getVisitTimestamp() + $previousVisit->getConsumingTime();
-                }
+            if ($visit->getVisitTimestamp() > $lastTS) {
+                $currentBlock = $lastTS;
+            } else {
+                $currentBlock = $visit->getVisitTimestamp();
             }
-            $futureVisits->next();
+
+            if (
+                $previousBlock >= $firstTS &&
+                $currentBlock <= $lastTS &&
+                ($currentBlock - $previousBlock) >= $consumingTime
+            ) {
+                return $previousBlock;
+            }
+
+            /** @var DSVisit $visit */
+            $previousVisit = $visit;
+
+            if (($visitEnd + $consumingTime) > $lastTS) {
+                break;
+            }
         }
 
-        if (($visit->getVisitTimestamp() + $visit->getConsumingTime() + $consumingTime) <= $lastTS) {
-            return $visit->getVisitTimestamp() + $visit->getConsumingTime();
+        if ($visitEnd < $lastTS) {
+            if ($visitEnd >= $firstTS && ($lastTS - $visitEnd) >= $consumingTime) {
+                return $visitEnd;
+            } else {
+                return $firstTS;
+            }
         }
 
         throw new VisitSearchFailure("Failed to find a visit in requested time range.", 500);
+    }
+
+    private function validateTimeRange(int $firstTS, int $lastTS, int $consumingTime): void
+    {
+        if ($firstTS >= $lastTS) {
+            throw new VisitSearchFailure("Failed to find a visit in requested time range.", 500);
+        }
+        if (($lastTS - $firstTS) < $consumingTime) {
+            throw new NeededTimeOutOfRange("Incorrect time range(" . strval($firstTS - $lastTS) . ") for requested time consumption(" . strval($consumingTime) . ".)", 500);
+        }
     }
 }
