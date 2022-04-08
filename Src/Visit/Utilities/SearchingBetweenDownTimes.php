ee3acc805f132c724280a9f75b68bac5d28772e0
Hirbod <hirbod.khatami.words@gmail.com>
2022-04-08 06:54:16 -0700

    Refactor SearchingBetweenDownTimes class

--- Src/Visit/Utilities/SearchingBetweenDownTimes.php
+++ Src/Visit/Utilities/SearchingBetweenDownTimes.php
@@ -6,6 +6,7 @@ use TheClinicDataStructures\DataStructures\Time\DSDownTimes;
 use TheClinicDataStructures\DataStructures\Visit\DSVisits;
 use TheClinic\Exceptions\Visit\NeededTimeOutOfRange;
 use TheClinic\Exceptions\Visit\VisitSearchFailure;
+use TheClinicDataStructures\DataStructures\Time\DSDownTime;
 
 class SearchingBetweenDownTimes
 {
@@ -21,8 +22,12 @@ class SearchingBetweenDownTimes
 
     public function search(int $firstTS, int $lastTS, DSVisits $futureVisits, DSDownTimes $dsDownTimes, int $consumingTime): int
     {
+        // For testing purposes
         $firstDT = (new \DateTime)->setTimestamp($firstTS);
         $lastDT = (new \DateTime)->setTimestamp($lastTS);
+
+        $this->validateTimeRange($firstTS, $lastTS, $consumingTime);
+
         $intruptingDSDownTimes = $this->dowTime->findDownTimeIntruptionWithTimeRange($firstTS, $lastTS, $dsDownTimes);
 
         if (count($intruptingDSDownTimes) === 0) {
@@ -32,28 +37,57 @@ class SearchingBetweenDownTimes
 
             /** @var \TheClinicDataStructures\DataStructures\Time\DSDownTime $dsDownTime */
             foreach ($newDSDownTimes as $dsDownTime) {
-                if ($dsDownTime->getStartTimestamp() <= $firstTS) {
+                if ($dsDownTime->getStartTimestamp() <= $firstTS && $dsDownTime->getEndTimestamp() >= $lastTS) {
+                    throw new VisitSearchFailure("Failed to find a visit in the requested time range.", 500);
+                }
+
+                if ($dsDownTime->getEndTimestamp() <= $firstTS) {
+                    continue;
+                } elseif ($dsDownTime->getStartTimestamp() <= $firstTS) {
+                    $firstTS = $dsDownTime->getEndTimestamp();
+                    $this->validateTimeRange($firstTS, $lastTS, $consumingTime);
                     continue;
                 }
 
-                if ($newDSDownTimes->key() === 0 && $dsDownTime->getStartTimestamp() > $firstTS && ($dsDownTime->getStartTimestamp() - $firstTS) >= $consumingTime) {
-                    return $firstTS;
+                if (isset($previousDSDownTime)) {
+                    $previousBlock = $previousDSDownTime->getEndTimestamp();
+                } else {
+                    $previousBlock = $firstTS;
                 }
 
-                $newDSDownTimes->prev();
-                if ($newDSDownTimes->valid() && $newDSDownTimes->current()->getEndTimestamp() > $firstTS) {
-                    $firstTS = $newDSDownTimes->current()->getEndTimestamp();
+                if ($dsDownTime->getStartTimestamp() > $lastTS) {
+                    $currentBlock = $lastTS;
+                } else {
+                    $currentBlock = $dsDownTime->getStartTimestamp();
                 }
-                $newDSDownTimes->next();
 
-                try {
-                    return $this->SearchingBetweenTimeRange->search($firstTS, $dsDownTime->getStartTimestamp(), $consumingTime, $futureVisits);
-                } catch (VisitSearchFailure $th) {
-                } catch (NeededTimeOutOfRange $th) {
+                if (
+                    $previousBlock >= $firstTS &&
+                    $currentBlock <= $lastTS &&
+                    ($currentBlock - $previousBlock) >= $consumingTime
+                ) {
+                    try {
+                        return $this->SearchingBetweenTimeRange->search($previousBlock, $currentBlock, $consumingTime, $futureVisits);
+                    } catch (VisitSearchFailure $th) {
+                    } catch (NeededTimeOutOfRange $th) {
+                    }
+                }
+
+                /** @var DSDownTime $previousDSDownTime */
+                $previousDSDownTime = $dsDownTime;
+
+                if ($dsDownTime->getEndTimestamp() > $lastTS) {
+                    break;
                 }
             }
 
             if ($dsDownTime->getEndTimestamp() < $lastTS) {
+                if ($dsDownTime->getEndTimestamp() >= $firstTS && ($lastTS - $dsDownTime->getEndTimestamp()) >= $consumingTime) {
+                    $previousBlock = $dsDownTime->getEndTimestamp();
+                } else {
+                    $previousBlock = $firstTS;
+                }
+
                 try {
                     return $this->SearchingBetweenTimeRange->search($dsDownTime->getEndTimestamp(), $lastTS, $consumingTime, $futureVisits);
                 } catch (VisitSearchFailure $th) {
@@ -63,5 +97,15 @@ class SearchingBetweenDownTimes
         }
 
         throw new VisitSearchFailure("Failed to find a visit in the requested time range.", 500);
+    }
+
+    private function validateTimeRange(int $firstTS, int $lastTS, int $consumingTime): void
+    {
+        if ($firstTS >= $lastTS) {
+            throw new VisitSearchFailure("Failed to find a visit in requested time range.", 500);
+        }
+        if (($lastTS - $firstTS) < $consumingTime) {
+            throw new NeededTimeOutOfRange();
+        }
     }
 }
